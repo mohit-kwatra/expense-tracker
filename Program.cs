1da87c7b49ccecbd44939f4ec7ddaee0c29d2a74
Mohit Kwatra <mohitkwatra118@gmail.com>
2025-08-19 11:58:36 +0530

    Add CORS policy and configure JsonSerializerOptions for controllers

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 
 using Microsoft.EntityFrameworkCore;
 using ExpenseTracker.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+	.AddJsonOptions(x => 
+		x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DevConnection")));
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowLocalhost5173", policy =>
+	{
+		policy.WithOrigins("http://localhost:5173")
+			.AllowAnyHeader()
+			.AllowAnyMethod();
+	});
+});
 
 var app = builder.Build();
 
@@ -23,9 +35,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
 app.UseRouting();
+
+app.UseCors("AllowLocalhost5173");
 
 app.UseAuthorization();
 
